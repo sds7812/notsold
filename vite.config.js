795f92bd0fb2ced7f27438ca4a59f3376d4ba459
sds7812 <ksmled@naver.com>
2025-06-30 00:19:31 +0900

    경로 변경 및 node_modules Git에서 제거

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// Vite 설정 파일입니다. (GitHub Pages 배포를 위해 base 경로를 지정합니다)
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/notsold/', // GitHub Pages 배포 시, 저장소명으로 base 경로를 지정해야 정상 라우팅됩니다.
   plugins: [vue()],
   server: {
     port: 3000,
